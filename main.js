6cad8c239524b173e0b432a6013d5e9d9569efc5
fotohabilaj <fotohabilaj@gmail.com>
2026-02-03 15:02:14 +0100

    human code

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+//contare da 1 a 100, log che dice fizz sui multipli di 3 ebuz su quelli di 5
+//fizzbuzz su quelli di 15
+//elementi necessari for, if else, n%3;5;15==0, log
 for(let i=1; i<101; i++){
     console.log(i);
     
