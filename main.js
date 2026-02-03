469b1a698ebc98b57edbf373d51a079683eb8bc2
fotohabilaj <fotohabilaj@gmail.com>
2026-02-03 15:14:16 +0100

    complete

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //contare da 1 a 100, log che dice fizz sui multipli di 3 ebuz su quelli di 5
 //fizzbuzz su quelli di 15
 //elementi necessari for, if else, n%3;5;15==0, log
+
 for(let i=1; i<101; i++){
     if(i % 3==0){
         if(i % 15==0){
@@ -9,8 +10,7 @@ for(let i=1; i<101; i++){
         }else{
            console.log(i, 'fizz'); 
         }
-        
-        
+           
     } else if(i % 5==0){
         if(i % 15==0){
             console.log(i,'fizzbuzz');
@@ -20,6 +20,7 @@ for(let i=1; i<101; i++){
         }
            
     }
+
     console.log(i);
     
 }
