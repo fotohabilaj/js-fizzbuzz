77d15cf1e12ae1f34194fb4bb5df9f20ccc331a9
fotohabilaj <fotohabilaj@gmail.com>
2026-02-03 15:06:54 +0100

    fizz-fizzbuzz complete

--- main.js
+++ main.js
@@ -2,6 +2,16 @@
 //fizzbuzz su quelli di 15
 //elementi necessari for, if else, n%3;5;15==0, log
 for(let i=1; i<101; i++){
+    if(i % 3==0){
+        if(i % 15==0){
+            console.log(i,'fizzbuzz');
+            
+        }else{
+           console.log(i, 'fizz'); 
+        }
+        
+        
+    }
     console.log(i);
     
 }
